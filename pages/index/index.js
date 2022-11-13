8d00c5f0a721a729b928c34afe8c9a716d7e6baa
winDayyyyyy <407137314@qq.com>
2022-11-13 23:17:52 +0800

    初步实现新闻首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,14 +5,65 @@ Page({
    * 页面的初始数据
    */
   data: {
+    //新闻轮播图
+    newsSwiper:[],
+    //活动轮播图
+    activtySwiper:[],
+    //d导航栏
+    tabs: ["新闻","活动"],
+    current:0
+  },
 
+  /**
+   * 
+   * 更改导航栏选中
+   */
+  tabSelect:function(e){
+    var current = e.currentTarget.dataset.id
+    this.setData({
+      current:current
+    })
+  },
+
+  /**
+   * 获取新闻轮播图
+   */
+  getNewsSwiperList(){
+    let that = this;
+    wx.request({
+      url: 'url',
+      success(res){
+        if(res.data.code===0){
+          that.setData({
+            newsSwiper:res.data.newsSwiperList
+          })
+        }
+      }
+    })
+  },
+
+  /**
+   * 获取活动轮播图
+   */
+  getActivitySwiperList(){
+    let that = this;
+    wx.request({
+      url: 'url',
+      success(res){
+        if(res.data.code===0){
+          that.setData({
+            activitySwiper:res.data.activitySwiperList
+          })
+        }
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    // this.getNewsSwiperList();
   },
 
   /**
