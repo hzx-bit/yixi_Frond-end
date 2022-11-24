5a826b59428abffc1c9ea936904e8654288dfc78
hzxdsg <3310087607@qq.com>
2022-11-24 18:01:36 +0800

    完成map页面

--- pages/map/map.js
+++ pages/map/map.js
@@ -1,22 +1,63 @@
 // pages/map/map.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
+    cloudURL:'cloud://cloud1-3gjatjl2f1e06dd4.636c-cloud1-3gjatjl2f1e06dd4-1315043544/map图片/',
     campus:["旗山校区","铜盘校区","集美校区","怡山校区","晋江校区","泉港校区"],
-    cloudURL:"cloud://cloud1-3gjatjl2f1e06dd4.636c-cloud1-3gjatjl2f1e06dd4-1315043544/map图片/",
-    type:"怡山校区",
-    location:[{top:"110rpx",left:"140rpx",title:"至诚楼"},{top:"160rpx",left:"120rpx",title:"福建省中心检验所"},{top:"190rpx",left:"380rpx",title:"计算机工程系院楼"}],
-    title:null
+    type:"旗山校区",
+    title:null,
+    location:[],
+    paragragh:[],
+    imgURL:""
+  },
+  getSpots(){
+    var that = this;
+    wx.cloud.callFunction({
+      name: 'getSpots',
+      data:{
+        title:this.data.type
+      },
+      success(res){
+            if(res.result.data.length)
+            {
+              that.setData({
+                location:res.result.data[0].data
+              })
+            }
+            else{
+              that.setData({
+                location:[]
+              })
+            }
+      },
+      
+    })
+  },
+  getIntroduction(){
+    var that = this;
+    wx.cloud.callFunction({
+      name: 'getIntroduction',
+      data:{
+        title:this.data.title
+      },
+      success(res){
+        console.log(res.result.data[0].image)
+        that.setData({
+          paragraph:res.result.data[0].data,
+          imgURL:res.result.data[0].image
+        })
+      }
+    })
   },
   mapchange(e){
     var ct=this.data.campus[e.detail.value];
     this.setData({
       type:ct,
       title:null
-    })
+    });
+    this.getSpots();
   },
   showinfo(e){
     if(!this.data.title||this.data.title!=e.currentTarget.dataset.title)
@@ -24,7 +65,7 @@ Page({
       this.setData({
         title:e.currentTarget.dataset.title
       });
-      console.log(this.data.title)
+      this.getIntroduction();
     }
     else{
       this.setData({
@@ -36,7 +77,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    this.getSpots();
   },
 
   /**
